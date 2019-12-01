2f92e49d3d7a36934c1fb1f765fa878b48c2d0c8
dranidis <dranidis@gmail.com>
2019-12-01 18:51:19 +0200

    Removed unneeded package

--- Restaurant.java
+++ Restaurant.java
@@ -3,7 +3,6 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 
 public class Restaurant {
     private List<Table> tables;
